d8deaa36ee7f3c1808fc5642575195e97168fa2d
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-14 23:49:59 +0200

    feat(auth_services): RequestAuthenticator service to check JWT in requests headers

---
+++ app/services/auth_services/request_authenticator.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module AuthServices
+  class RequestAuthenticator < ApplicationService
+    include Exceptions
+
+    attr_reader :headers
+
+    def initialize(headers)
+      super()
+      @headers = headers
+    end
+
+    def call
+      token = token_from_headers
+
+      decoded_payload = JsonWebToken.decode(token)
+      user_id = decoded_payload[:user_id]
+
+      User.find(user_id)
+    rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+      raise InvalidToken
+    end
+
+    private
+
+    def token_from_headers
+      token = @headers['Authorization']
+      raise MissingToken unless token
+
+      token
+    end
+  end
+end
