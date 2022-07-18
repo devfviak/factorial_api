5c640e79cc0372cdf0e2d1d78bc21b12645a27bf
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-18 12:08:25 +0200

    test(auth_services): User authenticator tests updated with new application base service definition

--- spec/services/auth_services/user_authenticator_spec.rb
+++ spec/services/auth_services/user_authenticator_spec.rb
@@ -10,20 +10,17 @@ RSpec.describe AuthServices::UserAuthenticator do
     let(:user) { create(:user) }
 
     context 'when valid credentials' do
-      subject(:valid_credentials_authenticator) { described_class.new(user.email, user.password) }
-
       it 'returns user jwt' do
-        user_jwt = valid_credentials_authenticator.call
-
+        user_jwt = described_class.call(user.email, user.password)
         expect(user_jwt).to eq token_generator(user.id)
       end
     end
 
     context 'when invalid credentials' do
-      subject(:invalid_credentials_authenticator) { described_class.new(user.email, "bad_#{user.password}") }
-
       it 'throws Exceptions::InvalidCredentials' do
-        expect { invalid_credentials_authenticator.call }.to raise_exception(Exceptions::InvalidCredentials)
+        expect do
+          described_class.call(user.email, "bad_#{user.password}")
+        end.to raise_exception(Exceptions::InvalidCredentials)
       end
     end
   end
