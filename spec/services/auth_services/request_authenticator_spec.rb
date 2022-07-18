73f212da99bbe33d50bb241f86c02964af120e71
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-18 12:11:35 +0200

    test(auth_services): RequestAuthenticator specs updated to match new application base service definition

--- spec/services/auth_services/request_authenticator_spec.rb
+++ spec/services/auth_services/request_authenticator_spec.rb
@@ -6,44 +6,38 @@ RSpec.describe AuthServices::RequestAuthenticator do
   # Test Suite for RequestAuthenticator#call
   describe '#call' do
     context 'when valid request' do
-      subject(:request_authenticator) { described_class.new(valid_headers) }
-
       let(:user) { create(:user) }
       let(:headers) { valid_headers }
 
       it 'returns user' do
-        user_result = request_authenticator.call
+        user_result = described_class.call(valid_headers)
         expect(user_result).to eq user
       end
     end
 
     context 'when missing token request' do
-      subject(:request_authenticator) { described_class.new({}) }
-
       let(:user) { create(:user) }
 
       it 'throws Exceptions::MissingToken' do
-        expect { request_authenticator.call }.to raise_exception(Exceptions::MissingToken)
+        expect { described_class.call({}) }.to raise_exception(Exceptions::MissingToken)
       end
     end
 
     context 'when fake token request' do
-      subject(:request_authenticator) { described_class.new({ 'Authorization' => 'foo' }) }
-
       let(:user) { create(:user) }
 
       it 'throws Exceptions::InvalidToken' do
-        expect { request_authenticator.call }.to raise_exception(Exceptions::InvalidToken)
+        expect { described_class.call({ 'Authorization' => 'foo' }) }.to raise_exception(Exceptions::InvalidToken)
       end
     end
 
     context 'when expired token request' do
-      subject(:request_authenticator) { described_class.new({ 'Authorization' => expired_token_generator(user.id) }) }
-
       let(:user) { create(:user) }
 
       it 'throws Exceptions::ExpiredToken' do
-        expect { request_authenticator.call }.to raise_exception(Exceptions::ExpiredToken)
+        expect do
+          described_class.call({ 'Authorization' => expired_token_generator(user.id) })
+        end.to raise_exception(Exceptions::ExpiredToken)
       end
     end
   end
