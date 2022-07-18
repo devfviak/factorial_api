d4aa0ddbb0e529ce9c8132dff1db8fd4e1af0a63
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-18 16:15:23 +0200

    Add some utils for application controller and exception handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
   include ExceptionHandler
+
+  private
+
+  def json_response(object, status = :ok)
+    render json: object, status:
+  end
 end
--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -6,21 +6,29 @@ module ExceptionHandler
   include Exceptions
 
   included do
+    # ActiveRecord related
     rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
+      json_response({ error: e.message }, :not_found)
     end
 
     rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
+      json_response({ error: e.message }, :unprocessable_entity)
     end
 
+    # Authentication related
     rescue_from InvalidToken, with: :unauthorized_response
     rescue_from ExpiredToken, with: :unauthorized_response
+    rescue_from MissingToken, with: :unauthorized_response
+
+    # Request Parameters related
+    rescue_from ActionController::ParameterMissing do |e|
+      json_response({ error: e.message }, :bad_request)
+    end
 
     private
 
     def unauthorized_response(exception)
-      json_response({ message: exception.message }, :unauthorized)
+      json_response({ error: exception.message }, :unauthorized)
     end
   end
 end
