1ff37736f725930dc16cff8c2ea5201386ddac14
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-18 17:04:52 +0200

    test: :truck: Move users spec outside v1 spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,14 +10,14 @@ RSpec.describe 'Users API', type: :request do
   let(:return_url) { 'http://google.com' }
 
   let(:headers) { valid_headers.except('Authorization') }
-  let(:valid_params) { { user: valid_attributes, return_url: }.to_json }
+  let(:valid_params) { { user: valid_attributes, return_url: } }
 
   describe 'POST /users' do
     context 'when valid signup request' do
-      before { post '/users', params: valid_params, headers: }
+      before { post '/users', params: valid_params, headers:, as: :json }
 
       it 'creates a new user' do
-        expect(user.errors).to be_empty
+        change(User, :count).by(1)
       end
 
       it 'returns an authentication token in cookies response' do
@@ -30,10 +30,14 @@ RSpec.describe 'Users API', type: :request do
     end
 
     context 'when invalid signup request with missing params' do
-      before { post '/users', params: {}, headers: }
+      before { post '/users', params: {}, headers:, as: :json }
 
       it 'responds with Bad Request error' do
         expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'does not create a new user' do
+        change(User, :count).by(1)
       end
 
       it 'returns nothing in cookies response' do
@@ -44,10 +48,14 @@ RSpec.describe 'Users API', type: :request do
     context 'when already existing user' do
       let(:user) { create(:user) }
 
-      before { post '/users', params: valid_params, headers: }
+      before { post '/users', params: valid_params, headers:, as: :json }
 
       it 'responds with Unprocessable Entity error' do
         expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'does not create a new user' do
+        change(User, :count).by(1)
       end
 
       it 'returns nothing in cookies response' do
