818dc5af1cac924b920e4196ce45fc87d3481bde
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-18 16:42:55 +0200

    feat(auth_services): AuthenticateController base class

---
+++ app/controllers/authenticated_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class AuthenticatedController < ApplicationController
+  before_action :authenticate_request
+
+  attr_reader :current_user
+
+  private
+
+  def authenticate_request
+    @current_user = AuthServices::RequestAuthenticator.call(req.headers)
+  end
+end
