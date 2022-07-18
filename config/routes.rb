7a943c261ba3db50a8b2092d6bf82a63e8c7767e
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-18 17:05:50 +0200

    feat(auth_services): Users controller with create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :companies
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resource :users, only: [:create]
 end
