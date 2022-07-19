a7cb6e2ec6b88cd0cab0dd9fbb7771b1c4e77ba2
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-19 10:32:51 +0200

    test: Include helper for auth cookie

--- spec/support/request_helper.rb
+++ spec/support/request_helper.rb
@@ -8,4 +8,8 @@ module RequestHelper
   def expired_token_generator(user_id)
     ::JsonWebToken.encode({ user_id: }, ttl: -1.second)
   end
+
+  def configure_auth_cookie(user_id)
+    cookies['auth_token'] = token_generator(user_id)
+  end
 end
