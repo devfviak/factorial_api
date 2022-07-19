2f13ed125e2d4e2e44daaf52894af9788efe6177
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-19 16:57:01 +0200

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_175538) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_19_102300) do
+  create_table "audit_logs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "action", null: false
+    t.json "audit_changes"
+    t.string "auditable_type"
+    t.bigint "auditable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["auditable_type", "auditable_id"], name: "index_audit_logs_on_auditable"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
@@ -22,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_175538) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone"], name: "index_users_on_phone", unique: true
   end
+
 end
