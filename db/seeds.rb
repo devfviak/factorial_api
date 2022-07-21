aa144955dc18f1242b787d237212221140367548
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-21 16:24:35 +0200

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Expense.destroy_all
+
+user = User.create!(first_name: 'Stan',
+										last_name: 'Smith',
+										email: 'stan@smith.com',
+										password: 'qwerty',
+										phone: '+34634234890')
+
+
+expense = Expense.create!(concept: 'Test expense',
+													subtotal_cents: 100,
+													total_cents: 120,
+													currency: 'USD',
+													processor: 'stripe',
+													user_id: user.id)
