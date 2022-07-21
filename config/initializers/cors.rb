d301ceb6138892bd41ba97990384b0991392abce
Pedro Ávila <pedro.avila@cleverppc.com>
2022-07-21 02:51:18 +0200

    Enable credentials for CORS middleware

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost'
+    origins [ENV.fetch('FRONTEND_URL')]
 
     resource '*',
              headers: :any,
-             methods: %i[get post put patch delete options head]
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
